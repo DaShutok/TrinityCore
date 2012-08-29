57951169067bc8f233c4cfbcb7090ea21b41bccd
Regigicas <regigicas@hotmail.es>
2012-08-29 13:17:44 +0200

    Core/Achievement: Fix quests timer Achievement
    Revert "Core/player: prevent player drink/eat while mount"
    This reverts commit 2661ec9eee9ffff76d7abefcfa7fcddaa067371e.

--- src/server/game/Entities/Player/Player.cpp
+++ src/server/game/Entities/Player/Player.cpp
@@ -1810,13 +1810,6 @@ void Player::Update(uint32 p_time)
     //because we don't want player's ghost teleported from graveyard
     if (IsHasDelayedTeleport() && isAlive())
         TeleportTo(m_teleport_dest, m_teleport_options);
-
-    //Prevent players drink/eat while mounted
-    if (HasAuraType(SPELL_AURA_MOUNTED))
-    {
-        RemoveAurasByType(SPELL_AURA_MOD_POWER_REGEN);
-        RemoveAurasByType(SPELL_AURA_MOD_REGEN);
-    }
 }
 
 void Player::setDeathState(DeathState s)
