f503acf639dd5ecbb154e315f48cd12290fb7934
Regigicas <regigicas@hotmail.es>
2012-09-09 17:51:30 +0200

    Forgot sql

---
+++ sql/dev/warden.sql
@@ -0,0 +1,3 @@
+DELETE FROM trinity_string WHERE entry = 20003;
+INSERT INTO `trinity_string` (`entry`, `content_default`, `content_loc1`, `content_loc2`, `content_loc3`, `content_loc4`, `content_loc5`, `content_loc6`, `content_loc7`, `content_loc8`) VALUES
+('20003','%sWarden System:%s%s %s : %s, %s. Comment: %s (CheckId: %u)%s',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
